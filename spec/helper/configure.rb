3b53e26871546ba1303fd9d2ed0279bc8607b8d2
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-11-22 17:02:30 +0100

    helper (spec) configure ensure image.id

--- spec/helper/configure.rb
+++ spec/helper/configure.rb
@@ -2,11 +2,15 @@
 
 # @type [Kamaze::DockerImage] image
 self.image.tap do |image|
-  RSpec.configure do |config|
-    config.before(:all) do
-      set(:os, family: :debian)
-      set(:backend, :docker)
-      set(:docker_image, image.id)
+  image.id.tap do |image_id|
+    raise "Can not retrieve id for image #{image.to_s.inspect} (ensure to build image fisrt)" if image_id.nil?
+  end.tap do |image_id|
+    RSpec.configure do |config|
+      config.before(:all) do
+        set(:os, family: :debian)
+        set(:backend, :docker)
+        set(:docker_image, image_id)
+      end
     end
   end
 end
